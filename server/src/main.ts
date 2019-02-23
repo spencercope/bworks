7644c015ee074297120f02abbe9aae103bce6357
Mohamed Langi <mlnp3@mail.umsl.edu>
2019-02-23 15:54:23 -0600

    heroku test

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import { join }  from 'path';
 import * as cors from 'cors';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
@@ -15,6 +16,9 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, expressApp);
   app.setGlobalPrefix('api');
+
+  const CLIENT_FILES = join(__dirname, '..', '..', 'client', 'dist');
+  app.use(express.static(CLIENT_FILES));
 
   await app.listen(process.env.PORT || 3000, () => {
     Logger.log('Server connected to port 3000', 'NestApplication');
